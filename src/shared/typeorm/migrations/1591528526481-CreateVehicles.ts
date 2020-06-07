c8ddd2edf3074dc8c7b7569aa47e19e692149f05
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-07 08:42:08 -0300

    Config ORM

---
+++ src/shared/typeorm/migrations/1591528526481-CreateVehicles.ts
@@ -0,0 +1,68 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateVehicles1591528526481 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'vehicles',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'model',
+            type: 'varchar',
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+          },
+          {
+            name: 'owner_id',
+            type: 'uuid',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'vehicles',
+      new TableForeignKey({
+        name: 'OwnerId',
+        columnNames: ['owner_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'owners',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('vehicles', 'OwnerId');
+    await queryRunner.dropTable('vehicles');
+  }
+}
