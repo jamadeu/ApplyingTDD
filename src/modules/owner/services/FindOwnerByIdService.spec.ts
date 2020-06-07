4f300ba7e2c8b738a172a451143cbb20c1f8b6a7
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-07 13:22:21 -0300

    Start tests of service FindOwnerById

---
+++ src/modules/owner/services/FindOwnerByIdService.spec.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppErrors';
+import FakeOwnerRepository from '@modules/owner/repositories/fakes/FakeOwnerRepository';
+import CreateOwnerService from './CreateOwnerService';
+import FindOwnerByIdService from './FindOwnerByIdService';
+
+let fakeOwnerRepository: FakeOwnerRepository;
+let createOwner: CreateOwnerService;
+let findOwnerById: FindOwnerByIdService;
+
+describe('FindOwnerById', () => {
+  beforeEach(() => {
+    fakeOwnerRepository = new FakeOwnerRepository();
+    createOwner = new CreateOwnerService(fakeOwnerRepository);
+    findOwnerById = new FindOwnerByIdService(fakeOwnerRepository);
+  });
+
+  it('be able to update a new owner', async () => {
+    const owner = await createOwner.execute({
+      name: 'Jhon Doe',
+      phone: '99-999999999',
+      email: 'jhondoe@example.com',
+    });
+
+    const findedOwner = await findOwnerById.execute({
+      owner_id: owner.id,
+    });
+
+    expect(findedOwner.name).toBe('Jhon Doe');
+    expect(findedOwner.phone).toBe('99-999999999');
+    expect(findedOwner.email).toBe('jhondoe@example.com');
+  });
+});
