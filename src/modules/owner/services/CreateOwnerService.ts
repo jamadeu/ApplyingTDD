708126e326ff7647f02c9467e247ddda1c5f1cbe
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-07 12:35:48 -0300

    Create service CreateOwnerService
    first test pass

---
+++ src/modules/owner/services/CreateOwnerService.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '@shared/errors/AppErrors';
+import IOwnerRepository from '@modules/owner/repositories/IOwnerRepository';
+import Owner from '@modules/owner/typeorm/entities/Owners';
+
+interface IRequest {
+  name: string;
+  phone: string;
+  email: string;
+}
+
+@injectable()
+class CreateOwnerService {
+  constructor(
+    @inject('OwnerRepository')
+    private ownersRepository: IOwnerRepository,
+  ) {}
+
+  public async execute({ name, phone, email }: IRequest): Promise<Owner> {
+    const userExists = await this.ownersRepository.findByEmail(email);
+    if (userExists) {
+      throw new AppError('Email already used');
+    }
+    const owner = await this.ownersRepository.create({ name, phone, email });
+    return owner;
+  }
+}
+
+export default CreateOwnerService;
