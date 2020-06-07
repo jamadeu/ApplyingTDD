3b8c860439f52d7f1e4d02019b77c7247b5c4bfd
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-07 10:50:45 -0300

    Create Owner entitie

---
+++ src/modules/owner/typeorm/entities/Owners.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('owners')
+class Owner {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Owner;
