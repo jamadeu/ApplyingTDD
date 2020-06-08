852877358b10e312cfe56caf76af5644c69cd781
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-08 07:08:24 -0300

    Create VehicleRepository

---
+++ src/modules/vehicle/typeorm/repositories/VehicleRepository.ts
@@ -0,0 +1,22 @@
+import { getRepository, Repository } from 'typeorm';
+import IVehicleRepository from '@modules/vehicle/repositories/IVehicleRepository';
+import Vehicle from '@modules/vehicle/typeorm/entities/Vehicle';
+import ICreateVehicleDTO from '@modules/vehicle/dtos/ICreateVehicleDTO';
+
+class VehicleRepository implements IVehicleRepository {
+  private ormRepository: Repository<Vehicle>;
+
+  constructor() {
+    this.ormRepository = getRepository(Vehicle);
+  }
+
+  public async create(data: ICreateVehicleDTO): Promise<Vehicle> {}
+
+  public async update(vehicle: Vehicle): Promise<Vehicle> {}
+
+  public async findById(id: string): Promise<Vehicle | undefined> {}
+
+  public async findByOwner(owner_id: string): Promise<Vehicle[]> {}
+}
+
+export default VehicleRepository;
