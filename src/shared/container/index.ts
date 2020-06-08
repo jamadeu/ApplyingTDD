37104ad01062e35672c7ce2fc34df061be1fe1ae
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-08 07:08:02 -0300

    Config dependency injection to VehicleRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IOwnerRepository from '@modules/owner/repositories/IOwnerRepository';
 import OwnerRepository from '@modules/owner/typeorm/repositories/OwnerRepository';
 
+import IVehicleRepository from '@modules/vehicle/repositories/IVehicleRepository';
+import VehicleRepository from '@modules/vehicle/typeorm/repositories/VehicleRepository';
+
 container.registerSingleton<IOwnerRepository>(
   'OwnerRepository',
   OwnerRepository,
+);
+
+container.registerSingleton<IVehicleRepository>(
+  'VehicleRepository',
+  VehicleRepository,
 );
