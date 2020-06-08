002f8e47f5b997e2f6b7ac22a41dcff192a4b37a
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-08 07:15:50 -0300

    Add 'todo' to VehicleRepository

--- src/modules/vehicle/typeorm/repositories/VehicleRepository.ts
+++ src/modules/vehicle/typeorm/repositories/VehicleRepository.ts
@@ -2,7 +2,7 @@ import { getRepository, Repository } from 'typeorm';
 import IVehicleRepository from '@modules/vehicle/repositories/IVehicleRepository';
 import Vehicle from '@modules/vehicle/typeorm/entities/Vehicle';
 import ICreateVehicleDTO from '@modules/vehicle/dtos/ICreateVehicleDTO';
-
+// TODO
 class VehicleRepository implements IVehicleRepository {
   private ormRepository: Repository<Vehicle>;
 
